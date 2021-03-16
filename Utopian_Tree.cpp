ca215f75c3778c2d6290b1080f99683180d2290c
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-03-16 16:53:26 +0530

    Create Utopian_Tree.cpp
    Utopian Tree
    
    HackerRank Problem Solution

---
+++ Utopian_Tree.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int utopianTree(int n)
+{
+    int x = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        if (i % 2 == 0)
+            x = x + 1;
+        else
+            x = x * 2;
+    }
+    return x;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++)
+    {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int result = utopianTree(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
